ec6c7f7ec732c874aaa53856f3687414a196d022
raj-humne <rajhumne3@gmail.com>
2025-12-07 18:41:25 +0530

    image animation problem

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('main'),
     smooth: true
 });
+var fixed = document.querySelector(".fixed-image")
+var elemC = document.querySelector(".elem-cont")
+elemC.addEventListener("mouseenter", ()=>{
+    alert("hehe")
+})
+elemC.addEventListener("mouseleave", ()=>{
+    fixed.style.display = "none"
+})
