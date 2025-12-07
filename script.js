1dcee85efd258f9b221c359f86446c257b066d41
raj-humne <rajhumne3@gmail.com>
2025-12-07 20:39:48 +0530

    page 3 completed

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('main'),
+//     smooth: true
+// });
 var fixed = document.querySelector(".fixed-image")
-var elemC = document.querySelector(".elem-cont")
-elemC.addEventListener("mouseenter", ()=>{
-    alert("hehe")
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(elem){
+    elem.addEventListener("mouseenter", ()=>{
+        fixed.style.display = "block"
+    })
+    elem.addEventListener("mouseleave", ()=>{
+        fixed.style.display = "none"
+    })
 })
-elemC.addEventListener("mouseleave", ()=>{
-    fixed.style.display = "none"
+var elems = document.querySelectorAll(".elem")
+elems.forEach((e)=>{
+    e.addEventListener("mouseenter", ()=>{
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
 })
